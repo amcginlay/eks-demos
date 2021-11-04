8981242ab039a548334c6304652b459a2563c298
Alan McGinlay <amcginla@3c22fb40feb5.ant.amazon.com>
2021-11-04 17:14:35 +0000

    intend to use http:// as part of the BACKEND environment variable

--- src/php-echo/index.php
+++ src/php-echo/index.php
@@ -3,6 +3,6 @@
   $ec2_instance = shell_exec('curl --silent http://169.254.169.254/latest/meta-data/instance-id');
   $ec2_ip = shell_exec('curl --silent http://169.254.169.254/latest/meta-data/local-ipv4');
   $localhost_ip = shell_exec('hostname -i | tr -d \'\n\'');
-  $backend_resp = (getenv("BACKEND") !== false ? shell_exec('curl --verbose http://' . getenv("BACKEND")) : "n/a");
+  $backend_resp = (getenv("BACKEND") !== false ? shell_exec('curl --verbose ' . getenv("BACKEND")) : "n/a");
   echo '{ "time": "' . date("H:i:s") . '", "version": "' . getenv("VERSION") . '", "ec2Instance": "' . $ec2_instance . '", "ec2IP": "' . $ec2_ip . '", "localhostIP": "' . $localhost_ip . '" , "backend": "' . $backend_resp . '" }' . "\n";
 ?>
