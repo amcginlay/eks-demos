1469c4eb2c57f11e747c5d0e180097d905afdf88
Alan McGinlay <amcginlay@users.noreply.github.com>
2021-11-29 21:37:12 +0000

    Update echo-server.go

--- doc/scratch/echo-server.go
+++ doc/scratch/echo-server.go
@@ -24,29 +24,29 @@ func shellExec(prg string, args ...string) string {
 }
 
 func getResponse() string {
-	time := time.Now().Format("15:04:05")
-	instanceId := shellExec("curl", "--silent", "http://169.254.169.254/latest/meta-data/instance-id")
-	ec2Ip := shellExec("curl", "--silent", "http://169.254.169.254/latest/meta-data/local-ipv4")
-	localHostIp := strings.TrimSuffix(shellExec("hostname", "-i"), "\n")
-	res := fmt.Sprintf(`{ "time": "%s", "version": "%s", "ec2Instance": "%s", "ec2IP": "%s", "localhostIP": "%s", "backend": "n/a" }`, time, version, instanceId, ec2Ip, localHostIp)
+    time := time.Now().Format("15:04:05")
+    instanceId := shellExec("curl", "--silent", "http://169.254.169.254/latest/meta-data/instance-id")
+    ec2Ip := shellExec("curl", "--silent", "http://169.254.169.254/latest/meta-data/local-ipv4")
+    localHostIp := strings.TrimSuffix(shellExec("hostname", "-i"), "\n")
+    res := fmt.Sprintf(`{ "time": "%s", "version": "%s", "ec2Instance": "%s", "ec2IP": "%s", "localhostIP": "%s", "backend": "n/a" }`, time, version, instanceId, ec2Ip, localHostIp)
     return res
 }
 
 func endpointHandler(w http.ResponseWriter, r *http.Request) {
     resp := getResponse()
-	log.Printf("%s", resp)
-	fmt.Fprintf(w, "%s\n", resp)
+    log.Printf("%s", resp)
+    fmt.Fprintf(w, "%s\n", resp)
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", endpointHandler)
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", endpointHandler)
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
 
-	log.Printf("Server listening on port %s", port)
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+    log.Printf("Server listening on port %s", port)
+    log.Fatal(http.ListenAndServe(":"+port, mux))
 }
